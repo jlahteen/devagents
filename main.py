9c15fad098c7560c946429022f60b5435879758b
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2024-11-22 21:43:55 +0200

    main developed further with trace files and ability to read prompts from files

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import datetime
+import os
+import sys
+
 from dotenv import load_dotenv
 from hello import say_hello
+from utils.coding_mode import start_coding_mode
+from utils.coding_mode import stop_coding_mode
 
 # Load the environment variables
 load_dotenv()
@@ -17,7 +23,7 @@ def main():
     # Create a Config instance
     config = Config()
 
-    # Create necessary agents
+    # Create the necessary agents
 
     # Developer Agent
     developer_agent = DeveloperAgent(config=config)
@@ -28,17 +34,69 @@ def main():
     # Writer Agent
     writer_agent = WriterAgent(config=config)
 
-    # Get the request from the user
-    user_prompt = "Hey! This is a software team of AI agents. How could we help you?\n"
-    coding_request = input(user_prompt)
-
     # Create a ScrumMaster agent
     scrum_master_agent = ScrumMasterAgent(
         config, developer_agent, reviewer_agent, writer_agent
     )
 
-    # Pass the customer request to the master agent
-    chat_result = scrum_master_agent.start_chat(coding_request)
+    # Get the prompt from the user
+    prompt = get_prompt()
+
+    # Start the coding mode
+    start_coding_mode()
+
+    # Redirect stdout
+    redirect_stdout()
+
+    # Pass the prompt to the ScrumMaster agent
+    chat_result = scrum_master_agent.start_chat(prompt)
+
+    # Restore stdout
+    restore_stdout()
+
+    # Stop the coding mode
+    stop_coding_mode()
+
+
+# Gets a prompt containing a coding task. The user can either enter a prompt
+# or enter a file path containg a prompt. In the latter case the file content
+# will be returned.
+def get_prompt():
+    user_input = input("Enter a prompt or a requirement file:\n")
+    prompt = ""
+
+    if os.path.isfile(user_input):
+        # The input is a valid file path
+        try:
+            with open(user_input, "r", encoding="utf-8") as file:
+                prompt = file.read()
+        except FileNotFoundError:
+            print("File was not found.")
+            sys.exit(1)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    else:
+        # The input is a prompt
+        prompt = user_input
+
+    return prompt
+
+
+# Redirects stdout to a trace file.
+def redirect_stdout():
+    trace_file = (
+        os.getenv("TRACE_DIR")
+        + "/trace-"
+        + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        + ".md"
+    )
+    sys.stdout = open(trace_file, "w", encoding="utf-8")
+
+
+# Restores stdout.
+def restore_stdout():
+    sys.stdout.close()
+    sys.stdout = sys.__stdout__
 
 
 if __name__ == "__main__":
