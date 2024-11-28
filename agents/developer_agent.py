e638dcc9569993d5fa943583742e2938a4bbfc67
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2024-11-28 20:18:25 +0200

    DeveloperAgent's system message moved to the DeveloperAgent class; system message also updated

--- agents/developer_agent.py
+++ agents/developer_agent.py
@@ -1,12 +1,32 @@
+import textwrap
 from config import Config
 from autogen import ConversableAgent
 
 
 class DeveloperAgent(ConversableAgent):
+    _system_message = textwrap.dedent(
+        """
+        You are a professional developer, known for reusable and maintainable code.
+        
+        You are specialist in several technologies such as .NET/C#, React and Python.
+        
+        You write almost bugless code which is highly appreciated among other developers.
+        
+        Add documentation for all essential places such as classes and methods. Add also
+        step-by-step comments to internal implementations in methods.
+        
+        When you write code, add the file name information for each file you write. Include also a
+        directory to the file names if a directory is mentioned in the request. If there is no
+        directory specified in the request, use the directory "output".
+        
+        You should improve the quality of your code based on the feedback from the reviewer.
+        """
+    )
+
     def __init__(self, config: Config):
         super().__init__(
             name="developer_agent",
-            system_message=config.developer_agent_system_message,
+            system_message=self._system_message,
             llm_config=config.llm_config,
         )
         self._code_execution_config = False
--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
-import textwrap
 
 
-# Class for centralized configuration management
+# A class for configuration.
 class Config:
     def __init__(self):
-        self.max_rounds = 12
+        self.max_rounds = 20
         self.llm_config = {
             "config_list": [
                 {
@@ -17,15 +16,3 @@ class Config:
                 }
             ]
         }
-        self.developer_agent_system_message = textwrap.dedent(
-            """
-            You are a professional C# developer, known for reusable and maintainable code.
-            You write almost bugless code which is highly appreciated among other developers.
-            Add XML documentation for all essential places such as classes and methods.
-            Add also step-by-step comments to internal implementations in methods.
-            When you generate code, add the file name information for each file you generate.
-            Include a directory to the file names if a directory is mentioned in the request.
-            If there is no directory specified in the request, use the directory "output".
-            You should improve the quality of your code based on the feedback from the reviewer.
-            """
-        )
