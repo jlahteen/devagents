652227fe3f13851ee12e743c26890004d4927a37
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2024-11-29 23:23:26 +0200

    File tool for saving files modified

--- tools/file_tools.py
+++ tools/file_tools.py
@@ -1,17 +1,19 @@
 import os
 
 
-# Saves code to a file. If the filename contains a directory and it does not
+# Saves a file content to a file path. If the file path contains a directory and it does not
 # exist, the method creates it.
-def save_code_to_file(filename: str, code: str) -> str:
+def save_file(file_path: str, file_content: str) -> str:
     try:
-        directory = os.path.dirname(filename)
+        directory = os.path.dirname(file_path)
         if directory and not os.path.exists(directory):
             os.makedirs(directory)
-        with open(filename, "w") as file:
-            file.write(code)
-        print(f"Code was saved to {filename}")
-        return filename
+        with open(file_path, "w") as file:
+            file.write(file_content)
+        print(f"save_file OK: Content was saved to '{file_path}'")
+        return file_path
     except Exception as e:
-        print(f"An error occurred while saving code to the file '{filename}': {e}")
+        print(
+            f"save_file ERROR: An error occurred while saving content to the file '{file_path}': {e}"
+        )
         return ""
