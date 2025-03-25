ad2bb099d2e21b89ee2e6f64e0fe057c757fbfdb
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-03-25 21:43:16 +0200

    Thread lock added to save_file tool

--- tools/file_tools.py
+++ tools/file_tools.py
@@ -1,5 +1,7 @@
 import os
+import threading
 
+file_lock = threading.Lock()
 
 def save_file(file_path: str, file_content: str) -> str:
     """
@@ -10,10 +12,14 @@ def save_file(file_path: str, file_content: str) -> str:
 
     try:
         directory = os.path.dirname(file_path)
-        if directory and not os.path.exists(directory):
-            os.makedirs(directory)
-        with open(file_path, "w") as file:
-            file.write(file_content)
+        # Allow only one thread to access the next section because of directory creation
+        with file_lock:
+            if directory and not os.path.exists(directory):
+                os.makedirs(directory)
+            with open(file_path, "w") as file:
+                file.write(file_content)
+                file.flush()
+                os.fsync(file.fileno())
         print(f"save_file OK: Content was saved to '{file_path}'")
         return file_path
     except Exception as e:
