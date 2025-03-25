29a7ac5bad33bdd77ff0aa680e955f36803aa322
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-03-25 18:19:03 +0200

    NewCodeOrchestratorAgent migrated to AutoGen 0.4

--- scenarios/new_code/new_code_orchestrator_agent.py
+++ scenarios/new_code/new_code_orchestrator_agent.py
@@ -1,15 +1,19 @@
 import textwrap
+from typing import Sequence
 from config import Config
-from autogen import UserProxyAgent, ConversableAgent, GroupChat, GroupChatManager
+from autogen_agentchat.teams import SelectorGroupChat
+from autogen_agentchat.conditions import TextMentionTermination
 from scenarios.orchestrator_agent_base import OrchestratorAgentBase
+from autogen_agentchat.ui import Console
+from autogen_agentchat.messages import AgentEvent, ChatMessage
 
 
 class NewCodeOrchestratorAgent(OrchestratorAgentBase):
-    """An orchestrator to run a NewCode scenario."""
+    """An orchestrator agent to run a NewCode scenario."""
 
     _system_message = textwrap.dedent(
         """
-        You are an orchestrator agent that manages the DevAgents team consisting of AI agents.
+        You are an orchestrator agent that manages the AI agents team to complete a coding task.
         """
     )
 
@@ -29,39 +33,30 @@ class NewCodeOrchestratorAgent(OrchestratorAgentBase):
         self._reviewer_agent = reviewer_agent
         self._output_agent = output_agent
 
-    def select_next_speaker(self, last_speaker, groupchat):
-        if last_speaker is self._user_proxy_agent:
-            return self._developer_agent
-        elif last_speaker is self._developer_agent:
-            return self._reviewer_agent
-        elif last_speaker is self._reviewer_agent:
-            if self._is_code_approved(groupchat.messages[-1]["content"]):
-                return self._output_agent
+    def select_next_speaker(self, messages: Sequence[AgentEvent | ChatMessage]):
+        if len(messages) == 1:
+            return self._developer_agent.name
+        elif messages[-1].source == self._developer_agent.name:
+            return self._reviewer_agent.name
+        elif messages[-1].source is self._reviewer_agent.name:
+            if self._is_code_approved(messages[-1].content):
+                return self._output_agent.name
             else:
-                return self._developer_agent
+                return self._developer_agent.name
         else:
             return None
 
-    def start_chat(self, coding_request):
-        # Create a user proxy agent
-        self._user_proxy_agent = UserProxyAgent(
-            "user_proxy_agent", human_input_mode="NEVER", code_execution_config=False
-        )
-        self.groupchat = GroupChat(
-            agents=[
-                self._user_proxy_agent,
+    async def start_chat(self, coding_request):
+        termination_condition = TextMentionTermination("TERMINATE")
+        self.groupchat = SelectorGroupChat(
+            [
                 self._developer_agent,
                 self._reviewer_agent,
                 self._output_agent,
             ],
-            speaker_selection_method=self.select_next_speaker,
-            messages=[],
-            max_round=self._config.max_rounds,
+            model_client=self._model_client,
+            selector_func=self.select_next_speaker,
+            max_turns=self._config.max_turns,
+            termination_condition=termination_condition,
         )
-        self.manager = GroupChatManager(
-            groupchat=self.groupchat, llm_config=self._config.llm_config
-        )
-        chat_result = self._user_proxy_agent.initiate_chat(
-            self.manager, message=coding_request
-        )
-        return chat_result
+        await Console(self.groupchat.run_stream(task=coding_request))
