7474abfd44031a1ebb8e686778b4b177ae812002
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-04-17 18:44:43 +0300

    shell_tools test refactored

---
+++ tests/shell_tools_tests.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+
+from tests.test_utils import copy_test_data
+from tools.shell_tools import run_command
+
+
+@pytest.mark.asyncio
+async def test_dotnet_build_should_build():
+    # Arrange
+    base_dir = os.getcwd()
+    test_data_dir = os.path.join(base_dir, "tests", "test_data", "hello_world_console_app")
+    test_run_dir = os.path.join(base_dir, "tests", "test_output", "test_dotnet_build")
+    copy_test_data(test_data_dir, test_run_dir)
+    os.chdir(test_run_dir)
+
+    # Act
+    result = run_command("dotnet build")
+
+    # Assert
+    assert os.path.exists(os.path.join(test_run_dir, "bin\\Debug\\net8.0\\HelloWorld.dll"))
+    assert os.path.exists(os.path.join(test_run_dir, "bin\\Debug\\net8.0\\HelloWorld.exe"))
+
+    # Clean up
+    os.chdir(base_dir)
