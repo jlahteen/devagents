2af9a61bcdb45fdc9d138cd39ff36ac85d8b4f90
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-04-17 20:34:54 +0300

    Test specific commands added as commented out

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,20 @@
 import pytest
 
-
 if __name__ == "__main__":
+    # Run all tests
     pytest.main(["-s", "tests"])
+
+    # BuldAgent tests
+    # pytest.main(["-s", "tests\\test_build_agent.py"])
+
+    # Shell tools tests
+    # pytest.main(["-s", "tests\\test_shell_tools.py"])
+
+    # Config tests
+    # pytest.main(["-s", "tests\\test_config.py"])
+
+    # DeveloperAgent tests
+    # pytest.main(["-s", "tests\\test_developer_agent.py"])
+
+    # ScaffoldAgent tests
+    # pytest.main(["-s", "tests\\test_scaffold_agent.py"])
