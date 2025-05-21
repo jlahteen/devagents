aeeba51bebf5ee7d664a51c7790170e348bba420
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-05-21 21:18:18 +0300

    close() method added

--- utils/misc.py
+++ utils/misc.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Tee:
     """A class to write to multiple streams simultaneously."""
 
@@ -11,3 +14,12 @@ class Tee:
     def flush(self):
         for stream in self.streams:
             stream.flush()
+
+    def close(self):
+        for stream in self.streams:
+            # Only close if the stream has a close method and is not sys.stdout/sys.stderr
+            if hasattr(stream, "close") and stream not in (sys.stdout, sys.stderr, sys.__stdout__, sys.__stderr__):
+                try:
+                    stream.close()
+                except Exception:
+                    pass
