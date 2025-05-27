67070af86e1866d6c1f2eacaeb076ff2ddf04ec9
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-05-27 17:34:49 +0300

    Test directories wrapped by to_os_path()

--- run_tests.py
+++ run_tests.py
@@ -1,35 +1,37 @@
 import pytest
 
+from tools.os_tools import to_os_path
+
 if __name__ == "__main__":
     # Run all tests
     pytest.main(["-s", "tests"])
 
     # BuldAgent tests
-    # pytest.main(["-s", "tests\\test_build_agent.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_build_agent.py")])
 
     # Shell tools tests
-    # pytest.main(["-s", "tests\\test_shell_tools.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_shell_tools.py")])
 
     # Config tests
-    # pytest.main(["-s", "tests\\test_config.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_config.py")])
 
     # DeveloperAgent tests
-    # pytest.main(["-s", "tests\\test_developer_agent.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_developer_agent.py")])
 
     # ScaffoldAgent tests
-    # pytest.main(["-s", "tests\\test_scaffold_agent.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_scaffold_agent.py")])
 
     # NewAppScenario tests
-    # pytest.main(["-s", "tests\\test_new_app_scenario.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_new_app_scenario.py")])
 
     # FixBuildScenario tests
-    # pytest.main(["-s", "tests\\test_fix_build_scenario.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_fix_build_scenario.py")])
 
     # TestAgent tests
-    # pytest.main(["-s", "tests\\test_test_agent.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_test_agent.py")])
 
     # Web tools tests
-    # pytest.main(["-s", "tests\\test_web_tools.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_web_tools.py")])
 
     # FixTestsScenario tests
-    # pytest.main(["-s", "tests\\test_fix_tests_scenario.py"])
+    # pytest.main(["-s", to_os_path("tests\\test_fix_tests_scenario.py")])
