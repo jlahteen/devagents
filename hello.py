d49cd673468ac9678699ba88b3a96f992e818e1a
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-05-30 20:46:11 +0300

    Home page link added to the hello output

--- hello.py
+++ hello.py
@@ -2,6 +2,16 @@ import pyfiglet
 
 VERSION = "Version 0.7"
 BUILD = "Build __BUILD__"
+HOME_PAGE = "https://github.com/jlahteen/devagents"
+
+
+def print_home_page(width):
+    """Prints the HOME_PAGE centered within the given width."""
+
+    home_page_link = f"\033[38;2;59;110;234m{HOME_PAGE}\033[0m"
+    left_padding = (width - len(HOME_PAGE)) // 2
+    right_padding = width - len(HOME_PAGE) - left_padding
+    print(f"/ {' ' * left_padding}{home_page_link}{' ' * right_padding} \\")
 
 
 def say_hello():
@@ -22,6 +32,7 @@ def say_hello():
     print(f"/ {''.ljust(width)} \\")
     print(f"/ {VERSION.center(width)} \\")
     print(f"/ {BUILD.center(width)} \\")
+    print_home_page(width)
     print(border_line)
     print(" ✨  Hey! We are a software team of AI agents.  ✨")
     print(" ✨       Let's create something together!      ✨")
