4fbf1aa1f6e53ca7b6a243cce53ab69c5030299f
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-06-06 20:02:42 +0300

    over_to() method added

--- utils/misc.py
+++ utils/misc.py
@@ -29,3 +29,10 @@ def print_yellow(text: str) -> None:
     """Prints a given text in yellow color to the console."""
 
     print(f"\033[93m{text}\033[0m")
+
+
+def over_to(agent_name: str) -> str:
+    """Prints a message indicating the next agent to speak."""
+
+    print_yellow(f"Over to {agent_name}...")
+    return agent_name
