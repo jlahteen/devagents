9272b855b101f8e252b3a4d38604b717aab46fd1
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-06-27 20:49:07 +0300

    Tee class removed (moved to another file)

--- utils/misc.py
+++ utils/misc.py
@@ -1,30 +1,6 @@
 import sys
 
 
-class Tee:
-    """A class to write to multiple streams simultaneously."""
-
-    def __init__(self, *streams):
-        self.streams = streams
-
-    def write(self, data):
-        for stream in self.streams:
-            stream.write(data)
-
-    def flush(self):
-        for stream in self.streams:
-            stream.flush()
-
-    def close(self):
-        for stream in self.streams:
-            # Only close if the stream has a close method and is not sys.stdout/sys.stderr
-            if hasattr(stream, "close") and stream not in (sys.stdout, sys.stderr, sys.__stdout__, sys.__stderr__):
-                try:
-                    stream.close()
-                except Exception:
-                    pass
-
-
 def print_yellow(text: str) -> None:
     """Prints a given text in yellow color to the console."""
 
