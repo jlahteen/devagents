91b5cb8cd58db30c77cbc86c1897a84a90581ea3
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-07-08 00:16:30 +0300

    processes folder added to build

--- deploy/build.bat
+++ deploy/build.bat
@@ -16,6 +16,7 @@ XCOPY ..\tools\* devagents\tools /I /Y /S
 XCOPY ..\scenarios\* devagents\scenarios /I /Y /S
 XCOPY ..\agents\* devagents\agents /I /Y /S
 XCOPY ..\monitoring\* devagents\monitoring /I /Y /S
+XCOPY ..\processes\* devagents\processes /I /Y /S
 
 REM Copy the tests
 MKDIR devagents\tests
