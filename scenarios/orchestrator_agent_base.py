2589b7c3ddba57da0ce79ed3dbd313216f5ef2ff
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-07-07 21:26:50 +0300

    run_team() and set_monitor() methods added

--- scenarios/orchestrator_agent_base.py
+++ scenarios/orchestrator_agent_base.py
@@ -1,4 +1,5 @@
 import re
+from abc import abstractmethod
 
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.conditions import TextMentionTermination
@@ -6,6 +7,7 @@ from autogen_core.models import ChatCompletionClient
 
 from agents.termination_agent import TerminationAgent
 from config import Config
+from monitoring.monitor import MonitorBase
 
 
 class OrchestratorAgentBase(AssistantAgent):
@@ -25,6 +27,17 @@ class OrchestratorAgentBase(AssistantAgent):
         self._config = config
         self._termination_agent = TerminationAgent(config=config)
         self._termination_condition = TextMentionTermination("TERMINATE")
+        self._monitor: MonitorBase = None
+
+    @abstractmethod
+    async def run_team(self, prompt: str):
+        """Runs the team with a given prompt."""
+        pass
+
+    def set_monitor(self, monitor: MonitorBase):
+        """Sets a monitor for the orchestrator agent."""
+
+        self._monitor = monitor
 
     def _is_code_approved(self, message):
         """Checks whether the code is approved in the given message."""
