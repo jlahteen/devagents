4dd4325205c528ae49c18027f20d02ee847ac0bd
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-08-05 20:04:49 +0300

    None finished_at value fixed

--- scenario_engine/scenario_engine.py
+++ scenario_engine/scenario_engine.py
@@ -1,4 +1,5 @@
 from scenario_engine.scenario_task import ScenarioTask, ScenarioTaskResult
+import datetime
 
 
 class ScenarioEngine:
@@ -17,7 +18,7 @@ class ScenarioEngine:
             return ScenarioTaskResult(
                 scenario_name=scenario_task.scenario_name,
                 started_at=scenario_task.started_at,
-                finished_at=scenario_task.finished_at,
+                finished_at=scenario_task.finished_at if scenario_task.finished_at else datetime.datetime.now(),
                 task_id=scenario_task.id,
                 workspace=scenario_task.workspace,
                 errors=[e],
