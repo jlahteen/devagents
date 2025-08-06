2718d0dd5f0ada7a1fd8a48f6fe02ceb6fa9cb55
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2025-08-06 19:07:49 +0300

    .env-linux renamed as .env.linux and added to .gitignore

--- deploy/run.bat
+++ deploy/run.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run -it -v C:\_temp\devagents:/devagents-ws --env-file ../.env-linux devagents
+docker run -it -v C:\_temp\devagents:/devagents-ws --env-file ../.env.linux devagents
