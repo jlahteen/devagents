69df91b4c1b573d22251928fd71736ff2aa380df
Juha Lähteenmäki <juha.lahteenmaki@live.com>
2026-03-20 20:44:27 +0200

    get_timestamp() added

--- tools/os_tools.py
+++ tools/os_tools.py
@@ -1,10 +1,15 @@
 import sys
+from datetime import datetime
+
+
+def get_timestamp() -> str:
+    """Returns the current date and time in the YYYY-MM-DD HH:MM format."""
+
+    return datetime.now().strftime("%Y-%m-%d %H:%M")
 
 
 def get_os_type() -> str:
-    """
-    Returns the current operating system type.
-    """
+    """Returns the current operating system type."""
 
     os_name = sys.platform.lower()
     if os_name.startswith("nt") or os_name.startswith("win"):
